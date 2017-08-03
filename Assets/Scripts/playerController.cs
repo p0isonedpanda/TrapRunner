1a711a662fa3c9337c12d5db87ae06ad9fbb7366
Daniel Coady <daniel.coady@outlook.com.au>
2017-08-03 15:22:10 +1000

    Initial commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 250.0f;
+    [SerializeField] float sprintSpeed = 500.0f;
+    [SerializeField] float jumpSpeed = 1000.0f;
+    [SerializeField] float lookSpeed = 10.0f;
+    [SerializeField] float gravity = 20.0f;
+    [SerializeField] GameObject playerCam;
+    Vector3 moveDir = Vector3.zero;
+    CharacterController charController;
+
+	// Use this for initialization
+	void Start()
+    {
+        charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            charController.Move(new Vector3(
+                Input.GetAxis("Horizontal") * sprintSpeed * Time.deltaTime,
+                0.0f,
+                Input.GetAxis("Vertical") * sprintSpeed * Time.deltaTime));
+        }
+        else
+        {
+            charController.Move(new Vector3(
+                Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime,
+                0.0f,
+                Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime));
+        }
+
+        if (charController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jumping");
+        }
+    }
+}
