896b76b11172edf1b613655bd13e488a2fcff42a
p0isonedpanda <daniel.coady@outlook.com.au>
2017-08-08 12:16:54 +1000

    Started on decals using particles

---
+++ Assets/Scripts/particleDecalData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleDecalData
+{
+    public Vector3 position, rotation;
+    public float size;
+    public Color color;
+}
---
+++ Assets/Scripts/particleDecalPool.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleDecalPool : MonoBehaviour
+{
+    public int maxDecals = 100;
+
+    int particleDecalDataIndex;
+    particleDecalData[] particleData;
+
+	void Start ()
+    {
+        particleData = new particleDecalData[maxDecals];
+        for (int i = 0; i < maxDecals; i++)
+        {
+            particleData[i] = new particleDecalData();
+        }
+	}
+	
+	void SepParticleData(ParticleCollisionEvent particleCollisionEvent)
+    {
+        if (particleDecalDataIndex >= maxDecals)
+        {
+            particleDecalDataIndex = 0;
+        }
+
+        particleDecalDataIndex++;
+    }
+}
